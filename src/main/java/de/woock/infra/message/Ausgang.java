5092600b4da454518153eeed3a92b74e35c5c539
Hermann Woock <Hermann.Woock@Web.de>
2023-08-11 11:19:02 +0200

    Vorgang weiter degeneralisiert

--- src/main/java/de/woock/infra/message/Ausgang.java
+++ src/main/java/de/woock/infra/message/Ausgang.java
@@ -7,7 +7,9 @@ import de.woock.domain.Abteilungen;
 import de.woock.domain.Anfrage;
 import de.woock.domain.Beschwerde;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @RequiredArgsConstructor
 @Component
 public class Ausgang {
@@ -15,11 +17,13 @@ public class Ausgang {
 	private final JmsTemplate ausgang;
 
     public void neuerVorgangFuerAbteilung(Anfrage anfrage, Abteilungen abteilung) {
-        ausgang.send(abteilung.name(), 
+        log.debug("Anfrage {} weitergeleitet", anfrage.getId());
+    	ausgang.send(abteilung.name(), 
                      session -> session.createObjectMessage(anfrage));
     }
     
     public void neuerVorgangFuerAbteilung(Beschwerde beschwerde, Abteilungen abteilung) {
+    	log.debug("Beschwerde {} weitergeleitet", beschwerde.getId());
     	ausgang.send(abteilung.name(), 
     			session -> session.createObjectMessage(beschwerde));
     }
--- src/main/java/de/woock/infra/message/Eingang.java
+++ src/main/java/de/woock/infra/message/Eingang.java
@@ -4,11 +4,14 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import de.woock.domain.Anfrage;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @Component
 public class Eingang {
 	@JmsListener(destination = "Antwort", containerFactory = "myFactory", subscription = "stattauto")
 	public void antwortVerarbeiten(Anfrage antwort) {
+		log.debug("Antbort eingegangen");
 		antwort.beantworten(antwort.getAntwort());
 	}	
 }
