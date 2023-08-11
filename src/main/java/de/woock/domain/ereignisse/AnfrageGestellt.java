3769bb2e6ff783019bc3a6e7da1256c7834c521a
Hermann Woock <Hermann.Woock@Web.de>
2023-08-11 17:02:23 +0200

    Ereignis zur gestellten Anfrage hinzugefügt

---
+++ src/main/java/de/woock/domain/ereignisse/AnfrageGestellt.java
@@ -0,0 +1,15 @@
+package de.woock.domain.ereignisse;
+
+import java.io.Serializable;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Data
+public class AnfrageGestellt implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private final Long   id;
+	private final String frage;
+	private String antwort;
+}
--- src/main/java/de/woock/infra/message/Ausgang.java
+++ src/main/java/de/woock/infra/message/Ausgang.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import de.woock.domain.Abteilungen;
 import de.woock.domain.Anfrage;
 import de.woock.domain.Beschwerde;
+import de.woock.domain.ereignisse.AnfrageGestellt;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -19,7 +20,7 @@ public class Ausgang {
     public void neuerVorgangFuerAbteilung(Anfrage anfrage, Abteilungen abteilung) {
         log.debug("Anfrage {} weitergeleitet: {}", anfrage.getId(), anfrage.getFrage());
     	ausgang.send(abteilung.name(), 
-                     session -> session.createObjectMessage(anfrage));
+                     session -> session.createObjectMessage(new AnfrageGestellt(anfrage.getId(), anfrage.getFrage())));
     }
     
     public void neuerVorgangFuerAbteilung(Beschwerde beschwerde, Abteilungen abteilung) {
