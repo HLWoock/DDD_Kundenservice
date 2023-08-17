fd39ed932afe8ce2e59e3f96a6a2a8516a61ee56
HLWoock <Hermann.Woock@web.de>
2023-08-17 18:05:01 +0200

    Exception von Stacktrace befreit

--- src/main/java/de/woock/domain/fehler/LeeresFeldFehler.java
+++ src/main/java/de/woock/domain/fehler/LeeresFeldFehler.java
@@ -1,12 +1,14 @@
 package de.woock.domain.fehler;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 public class LeeresFeldFehler extends Exception {
 
 	private static final long serialVersionUID = 1L;
 	private final String feld;
+	
+	public LeeresFeldFehler(String feld) {
+		super(String.format("%s darf nicht leer sein", feld), null, true, false);
+		this.feld = feld;
+	}
 	
 	public String feld() {
 		return feld;
