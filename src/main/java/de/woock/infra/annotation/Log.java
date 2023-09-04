1f800369c6483cca63e20c716d1d912c900d002e
Hermann Woock <Hermann.Woock@Web.de>
2023-09-04 16:01:57 +0200

    formatted

--- src/main/java/de/woock/infra/annotation/Log.java
+++ src/main/java/de/woock/infra/annotation/Log.java
@@ -1,13 +1,14 @@
 package de.woock.infra.annotation;
 
-import java.lang.annotation.ElementType;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Retention(RUNTIME)
+@Target(METHOD)
 public @interface Log {
-	boolean printParamsValues() default false;
-	String callMethodWithNoParamsToString() default "toString";
+	boolean printParamsValues()              default false;
+	String  callMethodWithNoParamsToString() default "toString";
 }
